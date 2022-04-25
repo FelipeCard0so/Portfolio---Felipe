3caf6222307745d57f6b0c60aeaaef98f9568142
FelipeCard0so <felipe.myb@gmail.com>
2022-04-25 15:02:14 -0300

    Minha tatuagem, para marcar meu início na carreira. Feita do no dia 21/04/2022 em Curitiba - PR, no antebraço direito.

---
+++ tatto.py
@@ -0,0 +1,12 @@
+# from vida import a.minha
+
+sucesso = " "
+
+while sucesso:
+    sucesso = str(input("Você já atingiu o Sucesso? "))
+    if sucesso != "sim":
+        print("Tente Novamente! ")
+    if sucesso == "sim":
+        print("Parabéns Continue Melhorar! ")
+
+        # break
