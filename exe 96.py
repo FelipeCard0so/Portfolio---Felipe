6c00c6fb9d89403fca653eea2e90ecb8b9b90fdf
FelipeCard0so <102364563+FelipeCard0so@users.noreply.github.com>
2022-05-08 19:55:51 -0300

    Exercicio de def, --> calculando a área de um terreno...

---
+++ exe 96.py
@@ -0,0 +1,12 @@
+def area(larg, comp):
+    a = larg * comp
+    print(f'A área do terreno {larg} x {comp} é de {a}m².')
+
+
+print('Controle de terreno')
+print('-' * 20)
+la = float(input('largura (m): '))
+co = float(input('comprimento (m): '))
+area(la, co)
+
+
