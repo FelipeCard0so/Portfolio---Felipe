a355726cead40ad4714a0d1a7685d5fe1f7752d7
FelipeCard0so <102364563+FelipeCard0so@users.noreply.github.com>
2022-05-08 20:12:01 -0300

    Exercicio de def, --> linhas que se adaptam ao tamanho do texto...

---
+++ exe. 97.py
@@ -0,0 +1,11 @@
+def escreva(msg):
+    tam = len(msg) + 4
+    print('~' * tam)
+    print(f"  {msg}")
+    print('~' * tam)
+
+
+escreva('Linhas que se adaptam ao tamanho do texto')
+escreva('Título principal')
+escreva('97')
+escreva('Olá Mundo')
